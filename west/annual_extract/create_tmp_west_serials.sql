5ea6b3589c3cce23c8d7abaac8de06f8cb1d2d5c
Andy Kohler <akohler@library.ucla.edu>
2017-09-25 15:48:21 -0700

    VBT-893: Update SQL to reflect latest criteria

--- west/annual_extract/create_tmp_west_serials.sql
+++ west/annual_extract/create_tmp_west_serials.sql
@@ -22,7 +22,16 @@ inner join ucladb.bib_master br on bt.bib_id = br.bib_id
 inner join ucladb.mfhd_master mm on bm.mfhd_id = mm.mfhd_id
 inner join ucladb.location l on mm.location_id = l.location_id
 where bt.bib_format = 'as'
-and substr(bt.field_008, 29, 1) = ' '
+and substr(bt.field_008, 24, 1) = ' ' -- form of item is not non-print
+and substr(bt.field_008, 29, 1) in (' ', 'u', '|') -- not gov doc, or can't tell
+and not exists (
+  select *
+  from vger_subfields.ucladb_bib_subfield
+  where record_id = bt.bib_id
+  and (tag like '074%' or tag like '086%')
+) -- filter out other gov docs not caught via 008
+and l.location_code != 'in' -- no internet holdings
+and upper(l.location_display_name) not like '%MICRO%' -- no microforms
 and br.suppress_in_opac = 'N'
 and mm.suppress_in_opac = 'N'
 and l.suppress_in_opac = 'N'
