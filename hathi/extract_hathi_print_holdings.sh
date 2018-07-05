19351c4614363612e29c67c012957cbb78f0c64a
Andy Kohler <akohler@library.ucla.edu>
2018-07-05 13:20:53 -0700

    Modify Hathi extract to combine UCLA/SRLF files per request 20180705

--- hathi/extract_hathi_print_holdings.sh
+++ hathi/extract_hathi_print_holdings.sh
@@ -7,27 +7,32 @@ DATE=`date "+%Y%m%d"` #YYYYMMDD
 SCHEMA=vger_report
 
 echo Creating supporting tables: create_tmp_clu_export_tables.sql...
-${VGER_SCRIPT}/vger_sqlplus_run $SCHEMA create_tmp_clu_export_tables.sql
+${VGER_SCRIPT}/vger_sqlplus_run ${SCHEMA} create_tmp_clu_export_tables.sql
 
 echo Creating supporting tables: create_tmp_hathi_base_table.sql...
-${VGER_SCRIPT}/vger_sqlplus_run $SCHEMA create_tmp_hathi_base_table.sql
+${VGER_SCRIPT}/vger_sqlplus_run ${SCHEMA} create_tmp_hathi_base_table.sql
 
 for FILE in srlf*.sql ucla*.sql; do
-  echo Running $FILE...
-  ${VGER_SCRIPT}/vger_sqlplus_run vger_report $FILE
-  mv $FILE.out `basename $FILE.out .sql.out`_$DATE.tsv
+  echo Running ${FILE}...
+  ${VGER_SCRIPT}/vger_sqlplus_run vger_report ${FILE}
+  mv ${FILE}.out `basename ${FILE}.out .sql.out`_${DATE}.tsv
 done
 
 echo Counting records in files...
 wc -l *.tsv
 
-echo Creating archives...
-zip -9 srlf_hathi_$DATE.zip srlf*.tsv
-zip -9 ucla_hathi_$DATE.zip ucla*.tsv
+echo Combining files...
+cat [su]*_serials*.tsv >> combined_serials_${DATE}.tsv
+cat [su]*_multi-part*.tsv >> combined_multi-part_${DATE}.tsv
+cat [su]*_single-part*.tsv >> combined_single-part_${DATE}.tsv
+rm [su]*.tsv
 
-echo If all OK, upload zip files to box.com
+echo Creating archive...
+zip -9 ucla_hathi_${DATE}.zip combined*.tsv
+
+echo If all OK, upload zip file to box.com
 echo and notify Hathi and CDL.
 echo See https://docs.library.ucla.edu/x/SJmqBw
 
 echo Dropping supporting tables...
-${VGER_SCRIPT}/vger_sqlplus_run $SCHEMA drop_tmp_hathi_tables.sql
+${VGER_SCRIPT}/vger_sqlplus_run ${SCHEMA} drop_tmp_hathi_tables.sql
